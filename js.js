dd38bb6cff1f80fdfd3f454d711882cb894265f4
Anthony <omageo54@gmail.com>
2026-09-21 12:19:33 +0100

    feat: implemented increment and decrement buttons

--- js.js
+++ js.js
@@ -32,3 +32,50 @@ containers.forEach(container => {
   img.addEventListener('click', selectThis);
   button.addEventListener('click', selectThis);
 });
+// const cartButtons = document.querySelectorAll('.add-to-cart');
+// cartButtons.forEach(function(button) {
+//   button.addEventListener('click', function() {
+//     cartButtons.forEach(function(b) {
+//       b.classList.remove('selected-2');
+//     });
+//     button.classList.add('selected-2');
+//   });
+// });
+
+const button = document.querySelector('.add-to-cart');
+
+button.addEventListener('click', function() {
+  button.classList.add('selected-2');
+  button.innerHTML = `
+    <img class="decrement" src="assets/images/icon-decrement-quantity.svg" alt="Decrease quantity">
+    <span class="quantity">1</span>
+    <img class="increment" src="assets/images/icon-increment-quantity.svg" alt="Increase quantity">
+  `;
+
+  const quantityDisplay = button.querySelector('.quantity');
+  let quantity = 1;
+
+  
+  button.querySelector('.increment').addEventListener('click', function(e) {
+    
+    e.stopPropagation();
+    quantity++;
+    quantityDisplay.textContent = quantity;
+  });
+
+  button.querySelector('.decrement').addEventListener('click', function(e) {
+    e.stopPropagation();
+    if (quantity > 1) {
+      quantity--;
+      quantityDisplay.textContent = quantity;
+    }
+  });
+});
+
+document.addEventListener('click', function(e) {
+  const clickedInsideACard = e.target.closest('.dessert-container-1');
+
+  if (!clickedInsideACard) {
+    
+  }
+});
