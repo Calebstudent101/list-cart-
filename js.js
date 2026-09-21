6eaa4daf76efc5af81d48a9085e4e9776eb7ebb5
Anthony <omageo54@gmail.com>
2026-09-21 14:04:54 +0100

    feat: added the js functionalities again and made it better

--- js.js
+++ js.js
@@ -1,64 +1,54 @@
 
-// const containers = document.querySelectorAll('.dessert-container-1');
+const containers = document.querySelectorAll('.dessert-container-1');
 
-// containers.forEach(container => {
-//   const img = container.querySelector('.dessert-img');
-//   const button = container.querySelector('.add-to-cart');
+containers.forEach(function(container) {
+  const img = container.querySelector('.dessert-img');
+  const button = container.querySelector('.add-to-cart');
+  const originalButtonHTML = button.innerHTML;
 
-//   function selectThis() {
-//     containers.forEach(c => {
-//       c.querySelector('.dessert-img').classList.remove('selected');
-//       c.querySelector('.add-to-cart').classList.remove('selected');
-//     });
-//     img.classList.add('selected');
-//     button.classList.add('selected');
-//   }
+  function activateStepper() {
+    img.classList.add('selected');
+    button.classList.add('selected-2');
 
-//   img.addEventListener('click', selectThis);
-//   button.addEventListener('click', selectThis);
-// });
+    button.innerHTML = `
+      <img class="decrement" src="assets/images/icon-decrement-quantity.svg" alt="Decrease quantity">
+      <span class="quantity">1</span>
+      <img class="increment" src="assets/images/icon-increment-quantity.svg" alt="Increase quantity">
+    `;
 
+    const quantityDisplay = button.querySelector('.quantity');
+    let quantity = 1;
 
-// const button = document.querySelector('.add-to-cart');
+    button.querySelector('.increment').addEventListener('click', function(e) {
+      e.stopPropagation();
+      quantity++;
+      quantityDisplay.textContent = quantity;
+    });
 
-// button.addEventListener('click', function() {
-//   button.classList.add('selected-2');
-//   button.innerHTML = `
-//     <img class="decrement" src="assets/images/icon-decrement-quantity.svg" alt="Decrease quantity">
-//     <span class="quantity">1</span>
-//     <img class="increment" src="assets/images/icon-increment-quantity.svg" alt="Increase quantity">
-//   `;
+    button.querySelector('.decrement').addEventListener('click', function(e) {
+      e.stopPropagation();
+      if (quantity > 1) {
+        quantity--;
+        quantityDisplay.textContent = quantity;
+      }
+    });
+  }
 
-//   const quantityDisplay = button.querySelector('.quantity');
-//   let quantity = 1;
+  img.addEventListener('click', activateStepper);
+  button.addEventListener('click', activateStepper);
+});
 
-  
-//   button.querySelector('.increment').addEventListener('click', function(e) {
-    
-//     e.stopPropagation();
-//     quantity++;
-//     quantityDisplay.textContent = quantity;
-//   });
+document.addEventListener('click', function(e) {
+  const clickedInsideACard = e.target.closest('.dessert-container-1');
 
-//   button.querySelector('.decrement').addEventListener('click', function(e) {
-//     e.stopPropagation();
-//     if (quantity > 1) {
-//       quantity--;
-//       quantityDisplay.textContent = quantity;
-//     }
-//   });
-// });
+  if (!clickedInsideACard) {
+    document.querySelectorAll('.dessert-img').forEach(function(img) {
+      img.classList.remove('selected');
+    });
 
-// document.addEventListener('click', function(e) {
-//   const clickedInsideACard = e.target.closest('.dessert-container-1');
-
-//   if (!clickedInsideACard) {
-//     document.querySelectorAll('.dessert-img').forEach(function(img) {
-//       img.classList.remove('selected');
-//     });
-
-//     document.querySelectorAll('.add-to-cart').forEach(function(button) {
-//       button.classList.remove('selected-2');
-//     });
-//   }
-// });
+    document.querySelectorAll('.add-to-cart').forEach(function(button) {
+      button.classList.remove('selected-2');
+      button.innerHTML = button.dataset.originalHtml;
+    });
+  }
+});
